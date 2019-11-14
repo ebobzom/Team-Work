975d41313a022bb09be4dc177a3a021496b55823
Elijah Bobzom <ebobzom@gmail.com>
2019-11-14 22:55:06 +0100

    :bug: fix no database error

--- table.js
+++ table.js
@@ -3,19 +3,19 @@ import pg from 'pg';
 const con = new pg.Client({
   user: 'postgres',
   host: 'localhost',
-  database: 'TeamWorkUsers',
+  database: 'teamwork',
   password: 'woody4real',
   port: 5432,
 });
 con.connect();
-con.query('DROP table if exists Users', (err, res) => {
+con.query('DROP table if exists Users', (err) => {
   if (err) console.log(err);
   console.log('ok');
 });
 
 const t1 = 'CREATE TABLE Users(user_id serial primary key not null, firstName varchar(15) not null, lastName varchar(15) not null, email varchar(25) not null, password varchar not null,';
 const t2 = 'gender varchar(12) not null, jobRole varchar(12) not null, department varchar(18) not null, address varchar(30) not null, isAdmin boolean not null, created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP)';
-con.query(t1 + t2, (err, res) => {
+con.query(t1 + t2, (err) => {
   if (err) console.log(err);
   console.log('ok');
   con.end();
