692f68aa193e0e631ff0919dd31bb30ea5e7effd
Elijah Bobzom <ebobzom@gmail.com>
2019-11-14 22:35:06 +0100

    bug: fix query string in table.js

--- table.js
+++ table.js
@@ -8,17 +8,15 @@ const con = new pg.Client({
   port: 5432,
 });
 con.connect();
-con.query('DROP table if exists Users');
-con.query(`CREATE TABLE Users(
-user_id serial primary key not null,
-firstName varchar(15) not null,
-lastName varchar(15) not null,
-email varchar(25) not null,
-password varchar not null,
-gender varchar(12) not null,
-jobRole varchar(12) not null,
-department varchar(18) not null,
-address varchar(30) not null,
-isAdmin boolean not null,
-created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
-)`);
+con.query('DROP table if exists Users', (err, res) => {
+  if (err) console.log(err);
+  console.log('ok');
+});
+
+const t1 = 'CREATE TABLE Users(user_id serial primary key not null, firstName varchar(15) not null, lastName varchar(15) not null, email varchar(25) not null, password varchar not null,';
+const t2 = 'gender varchar(12) not null, jobRole varchar(12) not null, department varchar(18) not null, address varchar(30) not null, isAdmin boolean not null, created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP)';
+con.query(t1 + t2, (err, res) => {
+  if (err) console.log(err);
+  console.log('ok');
+  con.end();
+});
